c63b6311bbf6e68db310bc50b129549b098c3da0
Devendra Kumar Singh <116472581+DevendrakumarGLAu@users.noreply.github.com>
2025-08-07 18:58:21 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { MainLayerComponent } from './main-layer/main-layer.component';
+import products from './data/products.json'; 
 
 export const routes: Routes = [
   {
@@ -12,13 +13,18 @@ export const routes: Routes = [
         loadComponent: () =>
           import('./product/product.component').then((m) => m.ProductComponent),
       },
-      { path: 'product/:slug', 
-         loadComponent: () =>
-         import('./product-details/product-details.component').then((m) => m.ProductDetailsComponent),
-       
-        data: { renderMode: 'dynamic' } 
-       },
-    ],
+      //{ path: 'product/:slug', 
+        // loadComponent: () =>
+       //  import('./product-details/product-details.component').then((m) => m.ProductDetailsComponent),
+       //
+       // data: { renderMode: 'dynamic' } 
+     //  },
+      
+    // assuming JSON is stored locally 
+      { path: 'products/:slug',
+       loadComponent: () => import('./product-details/product-details.component').then(m => m.ProductDetailsComponent), data: { renderMode: 'prerender' }, getPrerenderParams: () => { return products.map(product => { const slug = product.name .toLowerCase() .replace(/[^a-z0-9]+/g, '-') .replace(/^-+|-+$/g, '') + '-' + product.id; return { slug }; }); }
+      }
+  ],
   },
 ];
 
