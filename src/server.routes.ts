35a0aeed8aca668b37480e4e8ad1cac1eda0a70f
Devendra Kumar Singh <116472581+DevendrakumarGLAu@users.noreply.github.com>
2025-08-13 17:37:55 +0530

    Update server.routes.ts

--- src/server.routes.ts
+++ src/server.routes.ts
@@ -1,3 +1,8 @@
-export const prerenderConfig = {
-  excludedRoutes: ['/product/:id'],
-};
+import { ServerRoute, RenderMode } from '@angular/ssr';
+export const serverRoutes: ServerRoute[] = [
+  { path: '', renderMode: RenderMode.Server }, 
+  { path: 'products', renderMode: RenderMode.Server }, 
+  { path: 'products/:id', renderMode: RenderMode.Server
+    // 👈 SSR only, no prerendering 
+    }
+    ];
