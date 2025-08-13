7ea998416c2ee7798089a2c35ff479d48d865336
Devendra Kumar Singh <116472581+DevendrakumarGLAu@users.noreply.github.com>
2025-08-13 17:42:42 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,32 +13,11 @@ export const routes: Routes = [
         loadComponent: () =>
           import('./product/product.component').then((m) => m.ProductComponent),
       },
-      //{ path: 'product/:slug', 
-        // loadComponent: () =>
-       //  import('./product-details/product-details.component').then((m) => m.ProductDetailsComponent),
-       //
-       // data: { renderMode: 'dynamic' } 
-     //  },
-      { path: 'product/:id', 
-       loadComponent: () => import('./app/product-details/product-details.component').then(m => m.ProductDetailsComponent), 
-       data: { prerender: true }, 
-       getPrerenderParams: async () => { // Replace with real data fetch if needed
-         return [ 
-           { id: '1' },
-           { id: '2' },
-           { id: '3' }
-         ];
-       } },
-      
-    // assuming JSON is stored locally 
-      //{ path: 'products/:slug',
-    //   loadComponent: () => import('./product-details/product-details.component').then(m => m.ProductDetailsComponent), 
-      //  data: { renderMode: 'prerender' }, 
-      //  getPrerenderParams: () => { return products.map(product =>
-      //    { const slug = product.name.toLowerCase().replace(/[^a-z0-9]+/g, '-')
-      //      .replace(/^-+|-+$/g, '') + '-' + product.id; return { slug }; }); }
-      }
-  ],
+       {path: 'product/:id', 
+        loadComponent: () =>
+       import('./product-details/product-details.component').then((m) => m.ProductDetailsComponent),
+     },
+    ]
   },
 ];
 
