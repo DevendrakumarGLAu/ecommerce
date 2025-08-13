2d43a322598b875f5450886fb9cc2b18e3f724b0
Devendra Kumar Singh <116472581+DevendrakumarGLAu@users.noreply.github.com>
2025-08-13 17:24:57 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,10 +19,20 @@ export const routes: Routes = [
        //
        // data: { renderMode: 'dynamic' } 
      //  },
+      { path: 'product/:id', 
+       loadComponent: () => import('./app/product-details/product-details.component').then(m => m.ProductDetailsComponent), 
+       data: { prerender: true }, 
+       getPrerenderParams: async () => { // Replace with real data fetch if needed
+         return [ 
+           { id: '1' },
+           { id: '2' },
+           { id: '3' }
+         ];
+       } },
       
     // assuming JSON is stored locally 
-      { path: 'products/:slug',
-       loadComponent: () => import('./product-details/product-details.component').then(m => m.ProductDetailsComponent), 
+      //{ path: 'products/:slug',
+    //   loadComponent: () => import('./product-details/product-details.component').then(m => m.ProductDetailsComponent), 
       //  data: { renderMode: 'prerender' }, 
       //  getPrerenderParams: () => { return products.map(product =>
       //    { const slug = product.name.toLowerCase().replace(/[^a-z0-9]+/g, '-')
