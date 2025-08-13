4937de9e53ca1725cc3ca1b376d86a2537bca91c
Devendra Kumar Singh <116472581+DevendrakumarGLAu@users.noreply.github.com>
2025-08-13 17:54:03 +0530

    Update server.routes.ts

--- src/server.routes.ts
+++ src/server.routes.ts
@@ -2,7 +2,14 @@ import { ServerRoute, RenderMode } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   { path: '', renderMode: RenderMode.Server }, 
   { path: 'products', renderMode: RenderMode.Server }, 
-  { path: 'products/:id', renderMode: RenderMode.Server
-    // 👈 SSR only, no prerendering 
+  { path: 'products/:id', renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => { 
+      
+      [
+        { id: '1' }, 
+        { id: '2' }, 
+        { id: '3' } 
+      ];
+    }
     }
     ];
